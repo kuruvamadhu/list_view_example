6f35f7c53b556c0e536726795e5c1a84932852ba
unknown <kuruvamadhu993@gmail.com>
2023-11-17 07:10:23 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Trainer(models.Model):
+    trainer_name=models.CharField(max_length=100,primary_key=True)
+    subject=models.CharField(max_length=100)
+    age=models.IntegerField()
---
+++ app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView
+from app.models import *
+class Trainers_List(ListView):
+    model=Trainer
+
+    template_name='Trainers_List.html'
+    
+    
+    
+    context_object_name='tl'
+    #queryset=Trainer.objects.filter(trainer_name='rakesh')
+    ordering=['trainer_name']
+    
